905b401e35e9562b083d94c8388fa630b0957207
jessica-liddell <jessica04liddell@gmail.com>
2026-02-02 21:26:55 -0500

    Finishing yard_characteristics

--- 3 - Scripts/Exploratory.R
+++ 3 - Scripts/Exploratory.R
@@ -46,38 +46,42 @@ n_bre25 = num_observations(cleaned_breeding_data_2025)
 n_bre = num_observations(cleaned_breeding_data)
 
 
+
 # --- 1.2 OVERALL SPECIES RICHNESS --- #
 
 # Function to find SR from presence-absence matrices
 SR <- function(yard_wide){
-  yard_wide <- select(yard_wide,-any_of(c("Code","NA", "Unknown"))
+  yard_wide = select(yard_wide,-any_of(c("Code","NA", "Unknown"))
   )
   return(ncol(yard_wide))
 }
 
-
-
 # 1.21 Total
 total_yard_wide <- read_csv("4 - Outputs/SR matrices/total_yard_wide.csv")
 SR_total = SR(total_yard_wide)
-
+print(SR_total)
 
 # 1.22 Migration
-m24_yard_wide <- read_csv("4 - Outputs/SR matrices/m24_visit_wide.csv")
-m25_yard_wide <- read_csv("4 - Outputs/SR matrices/m25_visit_wide.csv")
-m_yard_wide <- read_csv("4 - Outputs/SR matrices/m_visit_wide.csv")
+m24_yard_wide <- read_csv("4 - Outputs/SR matrices/m24_yard_wide.csv")
+m25_yard_wide <- read_csv("4 - Outputs/SR matrices/m25_yard_wide.csv")
+m_yard_wide <- read_csv("4 - Outputs/SR matrices/m_yard_wide.csv")
 SR_mig24 = SR(m24_yard_wide)
+print(SR_mig24)
 SR_mig25 = SR(m25_yard_wide)
+print(SR_mig25)
 SR_mig = SR(m_yard_wide)
+print(SR_mig)
 
 # 1.23 Breeding
-b24_yard_wide <- read_csv("4 - Outputs/SR matrices/b24_visit_wide.csv")
-b25_yard_wide <- read_csv("4 - Outputs/SR matrices/b25_visit_wide.csv")
-b_yard_wide <- read_csv("4 - Outputs/SR matrices/b_visit_wide.csv")
+b24_yard_wide <- read_csv("4 - Outputs/SR matrices/b24_yard_wide.csv")
+b25_yard_wide <- read_csv("4 - Outputs/SR matrices/b25_yard_wide.csv")
+b_yard_wide <- read_csv("4 - Outputs/SR matrices/b_yard_wide.csv")
 SR_bre24 = SR(b24_yard_wide)
+print(SR_bre24)
 SR_bre25 = SR(b25_yard_wide)
+print(SR_bre25)
 SR_bre = SR(b_yard_wide)
-
+print(SR_bre)
 
 
 # --- 1.3 AVERAGE SPECIES RICHNESS PER YARD --- #
@@ -85,23 +89,31 @@ SR_bre = SR(b_yard_wide)
 # Function to find average species richness per yard
 avgSR_yard <- function(long_df,season){
   season_long = long_df[long_df$dataset==season,]
-  return(mean(season_long$richness))
+  avg_SR = mean(season_long$richness)
+  return(avg_SR)
 }
 
 SR_long <- read_csv("4 - Outputs/SR_long.csv")
 
 # 1.31 Total
 avgSR_yard_total = avgSR_yard(SR_long, season="total")
+print(avgSR_yard_total)
 
 # 1.32 Migration
 avgSR_yard_m24 = avgSR_yard(SR_long, season="mig_2024")
+print(avgSR_yard_m24)
 avgSR_yard_m25 = avgSR_yard(SR_long, season="mig_2025")
+print(avgSR_yard_m25)
 avgSR_yard_m = avgSR_yard(SR_long, season="mig_total")
+print(avgSR_yard_m)
 
 # 1.33 Breeding
 avgSR_yard_bre24 = avgSR_yard(SR_long, season="breed_2024")
+print(avgSR_yard_bre24)
 avgSR_yard_bre25 = avgSR_yard(SR_long, season="breed_2025")
+print(avgSR_yard_bre25)
 avgSR_yard_bre = avgSR_yard(SR_long, season="breed_total")
+print(avgSR_yard_bre)
 
 
 
@@ -109,7 +121,7 @@ avgSR_yard_bre = avgSR_yard(SR_long, season="breed_total")
 
 # Function to find average number of species seen in yard survey
 avgSR_visit <- function(visit_wide) {
-  visit_wide = select(visit_wide,-any_of(c(Code,Date,`NA`,Unknown))
+  visit_wide = select(visit_wide,-any_of(c("Code","Date","NA","Unknown"))
   )
   richness = rowSums(visit_wide)
   return(mean(richness))
@@ -118,7 +130,6 @@ avgSR_visit <- function(visit_wide) {
 # 1.41 Total
 total_visit_wide <- read_csv("4 - Outputs/SR matrices/total_visit_wide.csv")
 avgSR_visit_total = avgSR_visit(total_visit_wide)
-
 
 # 1.42 Migration
 m24_visit_wide <- read_csv("4 - Outputs/SR matrices/m24_visit_wide.csv")
@@ -134,18 +145,76 @@ b25_visit_wide <- read_csv("4 - Outputs/SR matrices/b25_visit_wide.csv")
 b_visit_wide <- read_csv("4 - Outputs/SR matrices/b_visit_wide.csv")
 avgSR_visit_bre24 = avgSR_visit(b24_visit_wide)
 avgSR_visit_bre25 = avgSR_visit(b25_visit_wide)
-avgSR_visit_bre = avgSR_visit(bre_visit_wide)
+avgSR_visit_bre = avgSR_visit(b_visit_wide)
 
 
 
 
 # ============================================================================ # 
-# 2. YARD HABITAT FEATURES  
+# 2. BIRD SPECIES PRESENCE ACROSS SEASONS (SPECIES-SPECIFIC)
 # ============================================================================ # 
 
-# --- 187. YARD HABITAT FEATURES --- #
+# --- 2.1 BIRD PRESENCE --- #
 
-# 1.1 Find mean, range, and SD of the following:
+# 2.11 List of bird species
+# Get species list, excluding Code, NA, and Unknown
+bird_code_list <- names(total_yard_wide)
+bird_code_list <- bird_code_list[bird_code_list != "NA"]
+bird_code_list <- bird_code_list[bird_code_list != "Unknown"]
+bird_code_list <- bird_code_list[bird_code_list != "Code"]
+print(sort(bird_code_list))
+
+# 2.12 Species presence in each season
+# Function to standardize the number of species in each season
+standardize <- function(df, species_list){
+  missing <- setdiff(species_list,names(df))
+  df[missing] <- 0 #set missing species to 0
+  df <- df[,species_list] # keep only species columns and same order
+  return(df)
+}
+
+# Standardize every season to have all 44 species
+m24_standardized = standardize(m24_yard_wide,bird_code_list)
+m25_standardized = standardize(m25_yard_wide,bird_code_list)
+b24_standardized = standardize(b24_yard_wide,bird_code_list)
+b25_standardized = standardize(b25_yard_wide,bird_code_list)
+
+# Find species present for each season
+total_pres <- as.integer(colSums(total_pres_abs) > 0)
+m24_pres <- as.integer(colSums(m24_standardized) > 0)
+m25_pres <- as.integer(colSums(m25_standardized) > 0)
+b24_pres <- as.integer(colSums(b24_standardized) > 0)
+b25_pres <- as.integer(colSums(b25_standardized) > 0)
+
+# Build species presence by season table
+species_pres_by_seasons <- data.frame(
+  species = bird_code_list,
+  m24 = m24_pres,
+  m25 = m25_pres,
+  b24 = b24_pres,
+  b25 = b25_pres
+)
+
+
+
+
+# --- 2.2 FREQUENCY OF PRESENCE --- #
+
+# 2.21 Number of yards in which it was present
+# Sum each column for the standardized data frames for each species
+
+
+
+
+
+
+# ============================================================================ # 
+# 3. YARD HABITAT FEATURES  
+# ============================================================================ # 
+
+# --- 3.1 SUMMARY STATISTICS --- #
+
+# 3.11 Find mean, range, and SD of the following:
       # •	Average area
       # •	Number of trees
       # •	Number of shrubs
@@ -175,8 +244,8 @@ summary_yard_features <- as.data.frame( # return data frame, not matrix
 )
 
 
-
-# 1.2 Find abundance of plant species in yards
+# --- 3.2 PLANT SPECIES --- #
+# 3.21 Find abundance of plant species in yards
 
 # Import data
 yard_trees_verified <- read_csv("1 - Input/yard_trees_verified.csv")
@@ -218,17 +287,6 @@ ggplot(species_abundance,
   ) +
   theme_bw()
 
-
-
-# --- 5. SPECIES-SPECIFIC DATA --- #
-
-# 5.1. Species presence in each season
-
-
-# 5.2 Frequency of presence (number of visits OR yards in which it was present)
-
-
-# 5.3 Number of yards in which it was present
 
 
 
--- 3 - Scripts/Yard_characteristics.R
+++ 3 - Scripts/Yard_characteristics.R
@@ -12,16 +12,13 @@ library(readr)
 library(tidyverse)
 
 # PROBLEMS I am currently having:
-# *area: how do I get the conversion to Kayleigh's yards?
-# **yards: Y50 is missing from the tree data, Y25 is missing from centroid data
+# **yards: Y50 is missing from the tree data
 # ***trees/shrubs: is the best way to differentiate them by stem number?
-# ****dbh: should i include shrubs in average DBH?
-# ******fruit: what are the fruiting species?
 
 # ============================================================================ # 
 # 1. YARD MEASUREMENTS
 # ============================================================================ # 
-# --- 1.1 CENTROID POINTS ** --- #
+# --- 1.1 CENTROID POINTS --- #
 # DEF: Find the middle-most point in each backyard in decimal degrees and UTM. 
 # (Note: front yards were not considered in this calculation because this would 
 # complicate the scaled calculations of yard features and so few of the 
@@ -34,18 +31,17 @@ library(tidyverse)
 
 # Import data here:
 centroid_data <- read_csv("~/Desktop/Jess_Honours/1 - Input/centroid_data.csv")
-# remove extra row that appeared in import
-centroid_data <- centroid_data[rowSums(is.na(centroid_data)) != ncol(centroid_data), ]
+# remove extra rows and columns that appeared in import
+centroid_data <- centroid_data[rowSums(is.na(centroid_data)) != ncol(centroid_data), 
+                               colSums(is.na(centroid_data)) != nrow(centroid_data)]
 
 
 
-# --- 1.2 AREA * ** --- #
-# DEF: Yard area calculated using Google Maps' Polygon Tool by visually drawing 
-# quadrilateral around yard. Included in centroid_data.
-# Area should also be found in Kayleigh's work, as this would also include front yards.
-  # NOTE: need Mackenzie's list of yards to accomplish this and compare
-back_area <- subset(centroid_data, select = c(Yard.Code, back_area_ha))
-
+# --- 1.2 AREA ** --- #
+# DEF: Backyard area calculated using Google Maps' Polygon Tool by visually drawing 
+# quadrilateral around yard, and total yard area (front yard included) from
+# Kayleigh's work.
+back_area <- subset(centroid_data, select = c(Yard.Code, area, back_area_ha))
 
 
 
@@ -54,11 +50,12 @@ back_area <- subset(centroid_data, select = c(Yard.Code, back_area_ha))
 # ============================================================================ # 
 
 ##### PROBLEM: NOA'S YARD HAS NO TREES OR SHRUBS??? ASK MACKENZIE
-##### ALSO: IS THE BEST WAY TO DIFFERENTIATE TREES AND SHRUBS BY THE NUMBER OF STEMS???
 
 # --- 2.1 NUMBER OF TREES AND SHRUBS PER YARD --- # ** ***
-# DEF: Count the number of trees (1 stem) and shrubs (>1 stem) in yards from 
-# yard_trees_verified data frame.
+# DEF: Count the number of trees and shrubs in yards from yard_trees_verified
+# I differentiated between trees and shrubs based on species, size, and stem number.
+# In particular, SYVU was defined as a tree if its DBH > 10 and stem = 1, and 
+# THOC was defined as a tree if DBH > 20 and stem = 1. 
 
 # Import yard_trees_verified:
 yard_plants_verified <- read_csv("~/Desktop/Jess_Honours/1 - Input/yard_trees_verified.csv")
@@ -66,15 +63,13 @@ yard_plants_verified <- read_csv("~/Desktop/Jess_Honours/1 - Input/yard_trees_ve
 # Write and use function that counts number of trees and shrubs in each yard
 count_trees_shrubs <- function(plant_df) {
   plant_df %>%
-    # classify trees and shrubs by number of stems
-    mutate(plant_type = if_else(Number.stems == 1, "tree_count", "shrub_count")) %>%
     # treat each combination of yard and plant type as its own group
-    group_by(Yard.Code, plant_type) %>%
+    group_by(Yard.Code, Type) %>%
     # collapse each group into one row
     summarise(n = n(), .groups = "drop") %>% # n: row count, "drop" removes grouping structure
     # reshape to yard-level
     pivot_wider(
-      names_from  = plant_type, # becomes column names
+      names_from  = Type, # becomes column names
       values_from = n, # fills column with n values
       values_fill = 0 # inserts 0 if there's no trees/shrubs
     )
@@ -84,26 +79,25 @@ tree_shrub_count <- count_trees_shrubs(yard_plants_verified)
 
 
 # --- 2.2 TREE AND SHRUB DENSITY --- #
-# DEF: Calculated the density of tree and shrubs in backyards based on their 
-# count yard area.
+# DEF: Calculated the density of vegetation (tree + shrubs) in backyards based on 
+# their area.
 
 # Join back_area with tree_shrub_count for density calculation
 area_and_veg_df <- back_area %>%
   left_join(tree_shrub_count, by = c("Yard.Code" = "Yard.Code"))
 
 # Write & use function to calculate density (count/area)
-calc_tree_shrub_density <- function(data, area_col = back_area_ha) {
+calc_density <- function(data, area_col=area) {
   data %>%
     mutate(
-      tree_density  = tree_count  / {{ area_col }},
-      shrub_density = shrub_count / {{ area_col }}
+      density = (tree + shrub) / {{ area_col }}
     )
 }
-density_df <- calc_tree_shrub_density(area_and_veg_df)
+density_df <- calc_density(area_and_veg_df, area_col=area)
 
 # Remove the back_area_ha, shrub, and tree columns
 density_df <- density_df %>%
-  select(-back_area_ha, -tree_count, -shrub_count)
+  select(-back_area_ha, -tree, -shrub, -area)
 
 
 
@@ -114,27 +108,19 @@ density_df <- density_df %>%
 # Function that finds DBH of trees, shrubs, and all plants from data frame
 mean_dbh_by_yard <- function(plant_df) {
   plant_df %>%
-    # Sort each row as a tree or shrubs based on number of stems
-    mutate(plant_type = if_else(Number.stems == 1, "tree", "shrub")) %>%
+    mutate(
+      # Convert any non-numeric values
+      DBH = as.numeric(DBH),
+      Number.stems = as.numeric(Number.stems),
+      adjusted_DBH = DBH/Number.stems) %>%
+    # Remove non-numeric values
+    filter(!is.na(adjusted_DBH),Number.stems>0) %>%
     # Group by yards
     group_by(Yard.Code) %>%
     # Find DBHs for all plants, trees, and shrubs
     summarise(
-      mean_dbh_all = mean(DBH, na.rm = TRUE),
-      
-      mean_dbh_tree = if_else( # If trees are present, find mean DBH
-        sum(plant_type == "tree") > 0,
-        mean(DBH[plant_type == "tree"], na.rm = TRUE),
-        NA_real_ # Else, NA
-      ),
-      
-      mean_dbh_shrub = if_else( # If shrubs are present, find mean DBH
-        sum(plant_type == "shrub") > 0,
-        mean(DBH[plant_type == "shrub"], na.rm = TRUE),
-        NA_real_ # Else, NA
-      ),
-      
-      .groups = "drop"
+      mean_dbh = mean(adjusted_DBH, na.rm=TRUE),
+      .groups="drop"
     )
 }
 mean_DBH_df <- mean_dbh_by_yard(yard_plants_verified)
@@ -143,49 +129,26 @@ mean_DBH_df <- mean_dbh_by_yard(yard_plants_verified)
 
 # --- 2.4 NUMBER OF FRUITING PLANTS--- # ******
 # DEF: Calculate the number of fruiting plants in each yard.
+# Information gathered from "Manual of Woody Landscape Plants" by Michael A. Dirr
+# Plants are considered fruiting if they produce fleshy fruit during spring or summer
 
 # Using the species listed in yard_plants_verified, determine which species are fruiting:
 unique(yard_plants_verified$Plant.sci) # get list of species
 
-# Determine which ones are fruiting based on research (Missouri Botanical Garden)
-fruiting_sp <- c("Amelanchier canadensis","Celtis occidentalis","Euonymus alatus",
-                 "Berberis thunbergii","Lonicera xylosteum","Magnolia soulangeana",
-                 "Malus baccata","Malus domestica","Malus sylvestris","Morus alba",
-                 "Prunus cerasus","Prunus communis","Prunus japonica","Pyrus communis",
-                 "Rhamnus cathartica","Sambucus nigra","Sorbus aucuparia","Taxus canadensis",
-                 "Tilia cordata") 
-                # Magnolia fruits in late summer/fall. 
-                # Check Tilia again (fruits don't seem to be good for birds)
-
-fruiting_sp <- c("Amelanchier canadensis", # ripe in June
-                 "Berberis thunbergii", # ripe in October and through winter
-                 "Lonicera xylosteum", # ripe in July and August
-                 "Malus baccata", # 
-                 "Malus domestica",
-                 "Malus sylvestris",
-                 "Morus alba", # ripe June to July
-                 "Prunus cerasus",#cerasifera, ripe June, July, August
-                 "Prunus communis",
-                 "Prunus japonica",
-                 "Pyrus communis",
-                 "Rhamnus cathartica",
-                 "Sambucus nigra",
-                 "Sorbus aucuparia",
-                 "Taxus canadensis",
-                 "Tilia cordata") # nutlet
-# "Euonymus alatus" fruits in fall
-# "Celtis occidentalis", nutlets persisting through fall
-# "Magnolia soulangeana" fruits in late summer/fall - and rarely and follicles
-# Check Tilia again (fruits don't seem to be good for birds)
-
-# Count the number of fruiting species in each yard:
-fruiting_count_by_yard <- yard_plants_verified %>%
-  mutate(is_fruiting = Plant.sci %in% fruiting_sp) %>%
-  group_by(Yard.Code) %>%
-  summarise(
-    n_fruiting_plants = sum(is_fruiting),
-    .groups = "drop"
-  )
+# Create function that counts fruiting plants in each season and overall per yard
+fruiting_count_by_yard <- function(plant_df){
+  plant_df %>%
+    mutate(
+      is_fruiting = Fleshy == "yes" &
+        Fruit_season %in% c("spring", "summer")
+    ) %>%
+    group_by(Yard.Code) %>%
+    summarise(
+      n_fruiting_plants = sum(is_fruiting, na.rm = TRUE),
+      .groups = "drop"
+    )
+}
+fruiting_df <- fruiting_count_by_yard(yard_plants_verified)
 
 
 
@@ -197,7 +160,7 @@ fruiting_count_by_yard <- yard_plants_verified %>%
 # script and stored in "SR_long.csv" and convert them to a wide data frame.
 
 # Import SR_long.csv for species richnesses
-SR_long <- read_csv("3 - Extraction/SR_long.csv")
+SR_long <- read_csv("4 - Outputs/SR_long.csv")
 
 # Convert into a wide data frame with the datasets as the columns and yards as rows
 richness_wide <- SR_long %>%
@@ -242,14 +205,14 @@ yard_characteristics <- Reduce(
     tree_shrub_count,
     density_df,
     mean_DBH_df,
-    count_big_trees,
-    fruiting_count_by_yard,
+    fruiting_df,
     richness_wide
   )
 )
 
 # Export yard_characteristics data frame
 write.csv(yard_characteristics, file="yard_characteristics.csv", row.names=FALSE)
+# Moved to "4 - Outputs" directory
 
 
 
