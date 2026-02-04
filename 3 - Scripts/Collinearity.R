f15662af74061955170f9f07a22d550d016a4948
jessica-liddell <jessica04liddell@gmail.com>
2026-02-04 10:20:54 -0500

    Yard habitat feature correlation matrix

--- 3 - Scripts/Collinearity.R
+++ 3 - Scripts/Collinearity.R
@@ -1,10 +1,51 @@
-# 03-Feb-2026                          
+# 04-Feb-2026                          
 # ============================================================================ # 
-#                            COLLINEARITY MATRIX
+#                             COLLINEARITY MATRICES
 # ============================================================================ # 
 
 # DESCRIPTION: 
-# 
+# Creating a correlation matrix to asses the collinearity of my yard-level and 
+# landscape-level habitat features
 
 # PACKAGES USED:
 library(readr)
+library(dplyr)
+library(corrplot)
+
+
+# ============================================================================ # 
+# 1. YARD HABITAT FEATURES  
+# ============================================================================ # 
+
+# --- 1.1 CORRELATION MATRIX --- #
+
+# 1.11 Prepare yard_characteristics to include only yard habitat features
+# Import data
+yard_characteristics <- read_csv("4 - Outputs/yard_characteristics.csv")
+
+# Remove yard ID, centroid data, and SR columns
+yard_features_only <- select(yard_characteristics, 
+                                c(area,back_area_ha,tree,shrub,density,mean_dbh,
+                                  n_fruiting_plants,n_native_plants))
+
+
+# 1.12 Create correlation matrices
+# Numerical correlation matrix
+cor_matrix_yard <- cor(yard_features_only) # numerical correlation matrix
+cor_matrix_yard
+
+corrplot(cor_matrix_yard) # visual correlation matrix
+
+
+
+# ============================================================================ # 
+# 2. LANDSCAPE HABITAT FEATURES  
+# ============================================================================ # 
+
+# --- 2.1 CORRELATION MATRIX --- #
+
+#cor_matrix_landscape
+
+
+
+
