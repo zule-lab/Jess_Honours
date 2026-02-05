46bc3001425452c677f36a366f708b2dc7e44587
jessica-liddell <jessica04liddell@gmail.com>
2026-02-04 22:31:07 -0500

    Preparing for GAMMs

---
+++ 3 - Scripts/08 - Yard_GAMMs.R
@@ -0,0 +1,15 @@
+# 04-Feb-2026                          
+# ============================================================================ # 
+#                                    YARD ~ SR GAMMS
+# ============================================================================ # 
+
+# DESCRIPTION: 
+# Creating Generalized Additive Mixed Models for yard-level habitat features and
+# species richness across seasons.
+
+# PACKAGES USED:
+library(readr)
+library(ggplot2)
+library(itsadug)
+library(mgcv)
+
