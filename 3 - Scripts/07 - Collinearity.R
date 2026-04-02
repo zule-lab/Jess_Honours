ae522eaf482b3508c302aa723202316c3222a8cf
jessica-liddell <jessica04liddell@gmail.com>
2026-04-02 11:00:06 -0400

    Landscape habitat feature (vegetation cover) collinearity

--- 3 - Scripts/07 - Collinearity.R
+++ 3 - Scripts/07 - Collinearity.R
@@ -45,4 +45,31 @@ corrplot(cor_matrix_yard) # visual correlation matrix
 
 # --- 2.1 CORRELATION MATRIX --- #
 
-#cor_matrix_landscape
+# 2.11 Prepare landscape_characteristics to include only yard habitat features
+# Import data
+landscape_characteristics <- read_csv("2 - Cleaned/landscape_characteristics.csv")
+
+# Remove yard ID, centroid data, and SR columns
+landscape_features_only <- landscape_characteristics %>% 
+  select(-Yard.Code, -SR_total, -SR_mig, -SR_mig_2024, -SR_mig_2025, -SR_breed, -SR_breed_2024, -SR_breed_2025)
+
+
+# 2.12 Create correlation matrices
+# Numerical correlation matrix
+cor_matrix_landscape <- cor(landscape_features_only) # numerical correlation matrix
+cor_matrix_landscape
+corrplot(cor_matrix_landscape) # visual correlation matrix
+
+# Conclusions:
+# - low canopy has high collinearity with everything
+# - high canopy also does
+# - the only acceptable variable is high_canopy_25m
+# This is probably because the buffers have a lot of spatial overlap with one another
+
+
+
+
+
+
+
+
